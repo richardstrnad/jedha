bfaf42a7ac8721402d5eb23a3f9dc93cf01d1ce0
Richard Strnad <git@strnad.ch>
2018-04-12 08:41:09 +0200

    Added new test input folder

---
+++ tests/sampleinput/ios_dict.py
@@ -0,0 +1,26 @@
+ios_dict = [
+  {
+    "device_id": "MyAp001",
+    "ip_address": "192.168.9.45",
+    "platform": "AIR-LAP1131G-E-K9",
+    "capabilities": "Trans-Bridge",
+    "local_port": "GigabitEthernet0/20",
+    "remote_port": "FastEthernet0"
+  },
+  {
+    "device_id": "NET001.domain.local",
+    "ip_address": "192.168.2.2",
+    "platform": "WS-C3850-24XS",
+    "capabilities": "Router Switch IGMP",
+    "local_port": "GigabitEthernet0/51",
+    "remote_port": "TenGigabitEthernet1/0/20"
+  },
+  {
+    "device_id": "NET002.domain.local",
+    "ip_address": "192.168.2.3",
+    "platform": "WS-C3850-24XS",
+    "capabilities": "Router Switch IGMP",
+    "local_port": "GigabitEthernet0/52",
+    "remote_port": "TenGigabitEthernet1/0/20"
+  }
+]
---
+++ tests/sampleinput/nxos_dict.py
@@ -0,0 +1,106 @@
+nxos_dict = [
+  {
+    "capabilities": "Switch IGMP",
+    "device_id": "NETS999999.domain.local",
+    "ip_address": "192.168.1.19",
+    "local_port": "GigabitEthernet2/0/23",
+    "platform": "WS-C3560-48PS",
+    "remote_port": "GigabitEthernet0/1"
+  },
+  {
+    "capabilities": "Switch IGMP",
+    "device_id": "NETL0033.domain.local",
+    "ip_address": "192.168.1.40",
+    "local_port": "GigabitEthernet2/0/22",
+    "platform": "WS-C3560G-48PS",
+    "remote_port": "GigabitEthernet0/2"
+  },
+  {
+    "capabilities": "Switch IGMP",
+    "device_id": "NETL0011.domain.local",
+    "ip_address": "192.168.1.5",
+    "local_port": "TenGigabitEthernet1/0/22",
+    "platform": "WS-C3850-12XS",
+    "remote_port": "TenGigabitEthernet1/0/1"
+  },
+  {
+    "capabilities": "Switch IGMP",
+    "device_id": "NETL0010.domain.local",
+    "ip_address": "192.168.1.4",
+    "local_port": "TenGigabitEthernet1/0/21",
+    "platform": "WS-C3850-12XS",
+    "remote_port": "TenGigabitEthernet1/0/1"
+  },
+  {
+    "capabilities": "Switch IGMP",
+    "device_id": "NETL0003.domain.local",
+    "ip_address": "192.168.1.21",
+    "local_port": "TenGigabitEthernet1/0/13",
+    "platform": "WS-C2960X-24PD-L",
+    "remote_port": "TenGigabitEthernet1/0/1"
+  },
+  {
+    "capabilities": "Switch IGMP",
+    "device_id": "NETL0004.domain.local",
+    "ip_address": "192.168.1.22",
+    "local_port": "TenGigabitEthernet1/0/14",
+    "platform": "WS-C2960X-24PD-L",
+    "remote_port": "TenGigabitEthernet1/0/1"
+  },
+  {
+    "capabilities": "Switch IGMP",
+    "device_id": "NETL0036.domain.local",
+    "ip_address": "192.168.1.43",
+    "local_port": "TenGigabitEthernet1/0/19",
+    "platform": "WS-C2960S-48FPS-L",
+    "remote_port": "GigabitEthernet1/0/49"
+  },
+  {
+    "capabilities": "Switch IGMP",
+    "device_id": "NETL0034.domain.local",
+    "ip_address": "192.168.1.41",
+    "local_port": "TenGigabitEthernet1/0/20",
+    "platform": "WS-C3560G-48PS",
+    "remote_port": "GigabitEthernet0/51"
+  },
+  {
+    "capabilities": "Router Switch IGMP",
+    "device_id": "NETG002.domain.local",
+    "ip_address": "192.168.5.3",
+    "local_port": "TenGigabitEthernet1/0/23",
+    "platform": "WS-C3850-24XS",
+    "remote_port": "TenGigabitEthernet1/0/23"
+  },
+  {
+    "capabilities": "Router Switch IGMP",
+    "device_id": "NETG002.domain.local",
+    "ip_address": "192.168.5.3",
+    "local_port": "TenGigabitEthernet1/0/24",
+    "platform": "WS-C3850-24XS",
+    "remote_port": "TenGigabitEthernet1/0/24"
+  },
+  {
+    "capabilities": "Host",
+    "device_id": "OtherDev01",
+    "ip_address": "192.168.2.108",
+    "local_port": "GigabitEthernet2/0/8",
+    "platform": "DS-C9120-K9",
+    "remote_port": "mgmt0"
+  },
+  {
+    "capabilities": "Host",
+    "device_id": "NETG007",
+    "ip_address": "192.168.1.8",
+    "local_port": "GigabitEthernet2/0/43",
+    "platform": "AIR-CT2504-K9",
+    "remote_port": "GigabitEthernet0/0/1"
+  },
+  {
+    "capabilities": "Host",
+    "device_id": "NETG007",
+    "ip_address": "192.168.1.8",
+    "local_port": "GigabitEthernet2/0/44",
+    "platform": "AIR-CT2504-K9",
+    "remote_port": "GigabitEthernet0/0/2"
+  }
+]
