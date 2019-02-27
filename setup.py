9e5b05c82dbf9aede8aebf638947947574a4fcbf
Richard Strnad <git@strnad.ch>
2019-02-27 13:41:26 +0100

    Setup.py cleanup

--- setup.py
+++ setup.py
@@ -4,21 +4,21 @@ jedha Installer using setuptools
 import os
 from setuptools import setup
 
+from jedha.__about__ import (__title__, __version__, __summary__, __author__,
+                             __email__)
+
 def read(fname):
+    '''
+    Reads the variables from the fname file
+    '''
     # Dynamically generate setup(long_description)
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-base_dir = os.path.dirname(__file__)
-
-about = {}
-with open(os.path.join(base_dir, 'jedha', '__about__.py')) as f:
-    exec(f.read(), about)
-
-setup(name=about['__title__'],
-      version=about['__version__'],
-      description=about['__summary__'],
-      author=about['__author__'],
-      author_email=about['__email__'],
+setup(name=__title__,
+      version=__version__,
+      description=__summary__,
+      author=__author__,
+      author_email=__email__,
       packages=['jedha'],
-      long_description=read('README.rst'),
-)
+      long_description=read('README.rst')
+     )
